af254670bb0a4faed79d71472fb6fa573c804124
tadpoleann <laesybab@gmail.com>
2021-12-30 21:14:24 -0800

    added typing animation header below navbar

---
+++ script.js
@@ -0,0 +1,6 @@
+//Load page content before the typing animation
+const typeAniText = document.querySelector(".top-container h1");
+
+window.onload = () => {
+  typeAniText.classList.add("greeting");
+};
