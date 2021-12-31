923f9aefeb44493158f81edbeececbcba2d51868
tadpoleann <laesybab@gmail.com>
2021-12-30 22:54:02 -0800

    added modal open and close functionality

--- script.js
+++ script.js
@@ -4,3 +4,31 @@ const typeAniText = document.querySelector(".top-container h1");
 window.onload = () => {
   typeAniText.classList.add("greeting");
 };
+
+// Modal Open and Close
+// open modal, close modal
+const projects = document.querySelectorAll("[data-target]");
+const close_btns = document.querySelectorAll(".close-btn");
+const overlay = document.querySelector("#overlay");
+
+projects.forEach((project) => {
+  project.addEventListener("click", () => {
+    document.querySelector(project.dataset.target).classList.add("active");
+    overlay.classList.add("active");
+  });
+});
+
+close_btns.forEach((close_btn) => {
+  close_btn.addEventListener("click", () => {
+    document.querySelector(close_btn.dataset.target).classList.remove("active");
+    overlay.classList.remove("active");
+  });
+});
+
+window.onclick = (e) => {
+  if (e.target == overlay) {
+    const modals = document.querySelectorAll(".modal");
+    modals.forEach((modal) => modal.classList.remove("active"));
+    overlay.classList.remove("active");
+  }
+};
